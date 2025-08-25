b2c3387b4dab6dea1e98400b893a300296d18777
SHRP <alex@sherpa.one>
2025-08-25 10:25:16 +0200

    add

--- src/array/01-add.ts
+++ src/array/01-add.ts
@@ -1,28 +1,35 @@
 const items = ["A", "B", "C"];
 
 // Add item to the end
-const itemsWithNewEnd = [...items, "D"];
+const itemsWithItemAtEnd = [...items, "D"];
 
-console.log(itemsWithNewEnd);
+console.log(itemsWithItemAtEnd);
 //["A", "B", "C", "D"]
 
 // Add item to the start
-const itemsWithNewStart = ["Z", ...items];
+const itemsWithItemAtStart = ["Z", ...items];
 
-console.log(itemsWithNewStart);
-// ["Z", "A", "B", "C"]
+console.log(itemsWithItemAtStart);
+//["Z", "A", "B", "C"]
 
-// Add many new items
-const itemsWithManyNewItems = [...items, "D", "E", "F"];
+// Add many new items at start
+const itemsWithManyNewItemsAtStart = [...items, "D", "E", "F"];
 
-console.log(itemsWithManyNewItems);
+console.log(itemsWithManyNewItemsAtStart);
 //["A", "B", "C", "D", "E", "F"]
 
-// New item at specific position
+// Add many new items at start
+const itemsWithManyNewItemsAtEnd = ["D", "E", "F", ...items];
+
+console.log(itemsWithManyNewItemsAtEnd);
+//["D", "E", "F", "A", "B", "C"];
+
+// Add new item at specific position
 const index = 2;
-const itemsWithTheNewItemInserted = [
+const itemsWithNewItemInsertedAtSpecificPosition = [
   ...items.slice(0, index),
   "New Item",
   ...items.slice(index)
 ];
-// Résultat : ["A", "B", "New Item", "C"]
+console.log(itemsWithNewItemInsertedAtSpecificPosition);
+//[ "A", "B", "New Item", "C" ]
